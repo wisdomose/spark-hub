b009dcf34a444eb115bbb9cfc56a15120eadcf8e
wisdomose <wisdomose05@gmail.com>
2024-03-03 03:51:47 +0100

    dashboard redirect error

--- src/store/bursar/store.tsx
+++ src/store/bursar/store.tsx
@@ -1,5 +1,6 @@
 import BursarService from "@/services/Bursar";
 import { Bursar, User } from "@/types";
+import { getApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
@@ -30,7 +31,7 @@ export default function BursarContextProvider({
     setUser(value);
   };
   const logout = async () => {
-    console.log("called")
+    console.log("called");
     setLoading(true);
     const auth = getAuth();
     await auth.signOut();
@@ -40,32 +41,35 @@ export default function BursarContextProvider({
   // fetch Bursar profile, set login and loading status
   useEffect(() => {
     const auth = getAuth();
+    const app = getApp();
 
-    const a = onAuthStateChanged(auth, (user) => {
-      console.log(user);
-      if (user) {
-        (async function () {
-          const bursar = new BursarService();
-          await bursar
-            .profile()
-            .then((res) => {
-              setUser(res);
-              setLoggedIn(true);
-            })
-            .catch((err) => {
-              toast.error("Failed to fetch bursar profile");
-            })
-            .finally(() => {
-              setLoading(false);
-            });
-        })();
-      } else {
-        setLoggedIn(false);
-        setLoading(false);
-      }
-    });
+    if (app) {
+      const a = onAuthStateChanged(auth, (user) => {
+        console.log(user);
+        if (user) {
+          (async function () {
+            const bursar = new BursarService();
+            await bursar
+              .profile()
+              .then((res) => {
+                setUser(res);
+                setLoggedIn(true);
+              })
+              .catch((err) => {
+                toast.error("Failed to fetch bursar profile");
+              })
+              .finally(() => {
+                setLoading(false);
+              });
+          })();
+        } else {
+          setLoggedIn(false);
+          setLoading(false);
+        }
+      });
 
-    return () => a();
+      return () => a();
+    }
   }, []);
 
   return (
